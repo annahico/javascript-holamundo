2a0c0596ab94a9677c8cb319dca70b45dd026a55
annahico <annahico@gmail.com>
2024-03-27 11:28:42 +0100

    finalizando tutorial tipado dinamico js

---
+++ js/04-tipado-dinamico.js
@@ -0,0 +1,11 @@
+let numero = 42;
+let nombre = "Hola Mundo";
+let verdadero = true;
+let undef;
+let nula = null;
+
+console.log(typeof nombre); //la variable TYPEOF indica el nombre de la variable
+console.log(typeof numero);
+console.log(typeof verdadero);
+console.log(typeof undef);
+console.log(typeof nula); // aparece como OBJECT porque la funcion retorna OBJECT a NULL por la implementación
