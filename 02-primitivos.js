6f8f975659732955a4beecea92ff06f30d339b15
annahico <annahico@gmail.com>
2024-03-27 11:09:02 +0100

    finalizando tutorial primitivos js

--- 02-primitivos.js
+++ 02-primitivos.js
@@ -1,1 +1,7 @@
-let numero = 1;
+let numero = 1;  //NUMBER
+let texto = "Hola Mundo"; //STRING
+let verdadero = true; //BOOLEAN
+let falso = false; //BOOLEAN
+let noDefinido; //aunque aparezca ANY en realidad es UNDEFINED
+let undef = undefined; // UNDEFINED puede ser un tipo de dato o valor
+let nulo = null; //NULL
