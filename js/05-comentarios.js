ea48688f416f38d5dd45020d8f9f9b201fd5bb0b
annahico <annahico@gmail.com>
2024-03-27 11:38:26 +0100

    finalizando tutorial comentarios js

--- js/05-comentarios.js
+++ js/05-comentarios.js
@@ -1,4 +1,17 @@
+/** asi hacemos
+ * comentarios de 
+ * muchas
+ * lineas, aunque esta es la version VSC
+ */
 
-let numero = 42;
+/*
+todo lo que escribamos
+entre medio de los
+slash y los asteriscos
+va a ser ignorado
+version JS
+*/
+
+let numero = 42; //esto es un comentario, AQUI, se anota la razón y el motivo
 
 console.log(numero);
