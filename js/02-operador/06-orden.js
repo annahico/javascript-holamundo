c56662b9411826b038118a05de78a4e5a0910d3e
annahico <annahico@gmail.com>
2024-03-31 12:52:16 +0200

    finalizando tutorial orden JS

---
+++ js/02-operador/06-orden.js
@@ -0,0 +1,5 @@
+// let resultado = 8/2*(2+2); //hay que poner * delante del parentesis
+// console.log(resultado);
+
+let resultado = 8 / (2 * (2 + 2)); //se pone un () a la operacion que queramos que se ejecute primero
+console.log(resultado);
