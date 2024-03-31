f21b83e543d72a32914a96ad05983b69e33ed7c8
annahico <annahico@gmail.com>
2024-03-31 13:00:26 +0200

    finalizando tutorial if JS

--- js/03-control-flujo/01-if.js
+++ js/03-control-flujo/01-if.js
@@ -1,4 +1,12 @@
-let edad = 15;
+// let edad = 15;
+
+// if (edad > 17) {
+//     console.log("Usuario mayor de edad")
+// }
+
+//NO MUESTRA NADA ARRIBA PORQUE ES MENOR
+
+let edad = 25;
 
 if (edad > 17) {
     console.log("Usuario mayor de edad")
