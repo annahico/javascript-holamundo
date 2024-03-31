fb842358ce551a659d46e985584ce7e418a5bf97
annahico <annahico@gmail.com>
2024-03-31 12:14:48 +0200

    finalizado tutorial comparacion JS

---
+++ js/02-operador/03-comparacion.js
@@ -0,0 +1,28 @@
+//OPERACIONES DE IGUALDAD
+
+// let a = 5;
+// console.log(a > 5);
+// console.log(a >= 5);
+// console.log(a < 5);
+// console.log(a <= 5);
+
+// let a = 10; 
+// console.log(a > 5);
+// console.log(a >= 5);
+// console.log(a < 5);
+// console.log(a <= 5);
+
+//OPERACIONES DE COMPARACION 
+
+let a = 10; 
+console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+console.log(a == 10);
+console.log(a != 10); //con el ! evalua si son diferentes
+//AHORA SE UTILIZA LOS SIMBOLOS DE ABAJO, NO LOS DE ARRIBA
+console.log(a == "10"); //igual al STRING de 10 el valor = pregunta los valores, NO los tipos
+console.log(a === "10"); //ponemos triple = para consultar valor y tipo
+console.log(a !== "10"); //preguntamos si son distinto tipo de dato y valor
