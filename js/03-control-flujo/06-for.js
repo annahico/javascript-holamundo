6461a90cd04737aecd7acec69fbd9f275c42ab7b
annahico <annahico@gmail.com>
2024-04-06 11:49:07 +0200

    finalizando tutorial for JS

--- js/03-control-flujo/06-for.js
+++ js/03-control-flujo/06-for.js
@@ -1,9 +1,21 @@
-let i = 2;
+// let i = 2; //inicializacion de la variable
 
-while (i < 10) {
+// while (i < 2) { //comparacion
+//     if (i % 2 == 0) {
+//         console.log("Número par", i);
+//     }
+//     i++; //incrementar el valor de i
+// }
+
+for (let i = 2; i < 10; i++) {
     if (i % 2 == 0) {
         console.log("Número par", i);
     }
-    i++;
 }
 
+/*se compone de tres expresiones:
+- inicializacion de la variable
+- comparacion
+- incrementar el valor de i
+
+*/
