eb11505214a5f3d51e1028c9a38d0815297ca43f
annahico <annahico@gmail.com>
2024-04-06 12:16:20 +0200

    finalizando tutorial switch JS

---
+++ js/03-control-flujo/10-switch.js
@@ -0,0 +1,12 @@
+let accion = "actualizar";
+
+switch (accion) {
+    case "listar":
+        console.log("Acción de listar");
+        break; //poner siempre un BREAK despues de un case
+    case "guardar":
+        console.log("Acción de guardar");
+        break;
+    default:
+        console.log("Accxión no reconocida");
+}
